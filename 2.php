6ad02f656ebf3eebd53505b62349a4f518bea8c2
Edi Koswara <edikoswara1st@gmail.com>
2021-04-17 20:06:03 +0700

    Update 2.php

--- 2.php
+++ 2.php
@@ -1,1 +1,18 @@
+<?php
 
+$datakey = array('dumb', 'ways', 'the', 'best');
+$word = "dumbways";
+
+function check($datakey, $word) {
+    for($i=0; $i<=count($datakey)-1; $i++) {
+        echo $datakey[$i] . " => ";
+        
+        if(strpos($word, $datakey[$i]) !== FALSE) {
+            echo "TRUE" . "\n";
+        } else {
+            echo "FALSE" . "\n";
+        }
+    }
+}
+
+check($datakey, $word);
