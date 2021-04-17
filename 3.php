e7660f3228df2a3550d9005f215b7c88851e4538
Edi Koswara <edikoswara1st@gmail.com>
2021-04-17 20:06:38 +0700

    Update 3.php

--- 3.php
+++ 3.php
@@ -1,1 +1,27 @@
+<?php
 
+function drawImage($n) {
+    // cek paramater adalah bilangan genap
+    if ($n % 2 == 0) {
+        
+        $baris = $n;
+        $kolom = $n;
+        
+        for ($i=0; $i<=$baris; $i++) {
+            for ($j=0; $j<=$kolom; $j++) {
+                if ($j % 2 == 0) {
+                    echo "#";
+                } else {
+                    echo "*";
+                }
+                    
+            }
+            echo "\n";
+        }
+        
+    } else {
+        echo 'Parameter bukan bilangan genap';
+    }
+}
+
+drawImage(10);
